5e7c5f49d99ce6abb2f687ddaad6758ae2307100
Romaric Zounlome <rzounlom@gmail.com>
2024-05-22 19:54:34 -0400

    setup react-toastify/react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
 
+import { Route, Switch } from "react-router-dom";
+
+import { Container } from "react-bootstrap";
+import HomePage from "./components/pages/home/HomePage";
+import SingleMoviePage from "./components/pages/single-movie/SingleMoviePage";
+import { ToastContainer } from "react-toastify";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <Container fluid className="App">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/movie/:id" component={SingleMoviePage} />
+      </Switch>
+    </Container>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
